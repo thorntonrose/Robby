a097fd000ba164c553d3102057dc52651ead612b
ethorro <thornton.rose@ericsson.com>
2016-03-23 15:22:28 -0400

    Shortened reply messages.

--- plugins/jenkins.py
+++ plugins/jenkins.py
@@ -26,6 +26,7 @@ def get_job_status(job_data, color):
    return JOB_STATUSES[color]
 
 def reply_error(message, method, url, resp):
+    log.debug("reply_error ...")
     message.reply("I got '{} {}' for '{} {}'.".format(resp.status_code, resp.reason, method, url))
 
 @respond_to("jenkins run (.*)", re.IGNORECASE)
@@ -45,7 +46,7 @@ def jenkins_run(message, job):
         log.debug("jenkins_run: status_code: {}".format(resp.status_code))
 
     if resp.status_code == 201:
-        message.reply("Jenkins job '{}' started. Check Slack or {} for results.".format(job, job_url))
+        message.reply("{} started: {}".format(job, job_url))
     else:
         reply_error(message, "POST", build_url, resp)
 
@@ -63,7 +64,6 @@ def jenkins_check(message, job):
        status = get_job_status(job_data, color)
        last_build_url = job_data["lastBuild"]["url"]
        log.debug("jenkins_check: color: {}, status: {}, last_build_url: {}".format(color, status, last_build_url))
-       message.reply("Jenkins job '{}' is marked '{}'. Check {} for information on the last build.".format( \
-           job, status, last_build_url))
+       message.reply("{} status: {}, last build: {}".format(job, status, last_build_url))
     else:
         reply_error(message, "GET", status_url, resp)
--- robby.py
+++ robby.py
@@ -4,13 +4,6 @@ import sys
 from slackbot.bot import Bot
 
 if __name__ == "__main__":
-    # logging.basicConfig(**{
-    #     'format': '[%(asctime)s] %(message)s',
-    #     'datefmt': '%m/%d/%Y %H:%M:%S',
-    #     'level': logging.INFO,
-    #     'stream': sys.stdout,
-    # })
-
     logging.config.fileConfig("logging.ini")
     logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(logging.WARNING)
     log = logging.getLogger(__name__)
